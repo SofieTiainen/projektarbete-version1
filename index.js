da6b039e93cc6e9ebe6b00454870ede937e8cfdf
SofieTiainen <tiainen77@hotmail.com>
2022-10-20 23:07:14 +0200

    thirdcommit

--- index.js
+++ index.js
@@ -10,54 +10,74 @@ switchColorBtn.addEventListener("click", () => {
     }
 });
 
-
-// let quizAnswers = ["1. brun", "1. svart", "1. vit", "2. frukt", "2. grönsaker", "2. mjölk", "3. matta", "4. frukt", "5. frukt", "6. 500 gram", "7. sant", "8. sant", "9. sant", "10. sant"];
+// let quizAnswers = [
+//     {
+//         correctsvar1: "1. brun",
+//         correctsvar2: "1. svart"
+//     },
+//     {
+//         correctsvar3: "2. frukt",
+//         correctsvar4: "2. grönsaker"
+//     },
+//     "3. matta", "4. frukt", "5. frukt", "6. 500 gram", "7. sant", "8. sant", "9. sant", "10. sant"
+// ];
 
 let quizAnswers = [
     {
+        question: "Vilken färg på päls kan hundar ha?",
         correctsvar1: "1. brun",
-        correctsvar2: "1. svart",
-        correctsvar3: "1. vit"
+        correctsvar2: "1. svart"
     },
     {
-        correctsvar4: "2. frukt",
-        correctsvar5: "2. grönsaker",
-        correctsvar6: "2. mjölk"
+        question: "Vad kan man hitta i en mataffär?",
+        correctsvar3: "2. frukt",
+        correctsvar4: "2. grönsaker"
+    },
+    {
+        question: "Vad kan man hitta i en möbelbutik?",
+        answer: "3. matta"
+    },
+    {
+        question: "Vad är en avocado?",
+        answer: "4. frukt"
+    },
+    {
+        question: "Vad är en paprika?",
+        answer: "5. frukt"
+    },
+    {
+        question: "Hur mycket grönsaker behöver vi äta per dag?",
+        answer: "6. 500 gram"
+    },
+    {
+        question: "Åsnor är dumma!",
+        answer: "7. sant"
+    },
+    {
+        question: "Hundar är snälla!",
+        answer: "8. sant"
+    },
+    {
+        question: "Kött påverkar miljön negativt!",
+        answer: "9. sant"
+    },
+    {
+        question: "Väte består av 5 protoner!",
+        answer: "10. falskt"
     }
 ];
 
-console.log(quizAnswers)
 
-// let newArr = [];
-
-// quizAnswers.forEach((obj) => {
-//     newArr.push(obj);
-//     console.log(newArr)
-// })
-//fungerade
-
-
-/*  {
-    namn: "Johan Johansson",
-    utbildning: "Frontend",
-    stad: "Stockholm",
-    år: 2
-  },*/
-
-
-let allQuestions = document.querySelector("#allQuestions"); //ej använt
 let showResultBtn = document.querySelector("#showResult");
 let results = document.querySelector("#results");
-let longResults = document.querySelector("#longresults");
-// let checkboxes = document.querySelectorAll("input[name='answer'][name='answer']");
-// let checkboxes = document.querySelectorAll("div.question1, div.question2");
+
 
 
 let runFiltering = () => {
     let allCheckedInputs = document.querySelectorAll("input[type='checkbox'][name='answer']:checked");
     let allCheckedRadios = document.querySelectorAll("[type='radio']:checked");
-
     let inputValues = [];
+
     allCheckedInputs.forEach((input) => {
         inputValues.push(input.value);
     });
@@ -65,29 +85,24 @@ let runFiltering = () => {
         inputValues.push(input.value);
     })
 
-    // quizAnswers.forEach((object) => {
-    //     console.log(object)
-    // })
-    //vå får ut de korrekta svaren var för sig
     let totalPoints = 0;
+
     quizAnswers.forEach((object) => {
-        console.log(object)
         let p = document.createElement('p')
-        results.innerText = "";
-        if (inputValues.includes(object)) {
+        p.innerText = `Question: ${object.question}`
+        if (inputValues.includes(object.answer) || (inputValues.includes(object.correctsvar1)) && (inputValues.includes(object.correctsvar2)) || (inputValues.includes(object.correctsvar3)) && (inputValues.includes(object.correctsvar4))) {
             totalPoints++
-            p.innerText = `You got ${totalPoints}/10 points!`
+            p.innerText += ` You answered right on this question!`
         } else {
-            p.innerText = `You got ${totalPoints}/10 points!`
+            p.innerText += ` You answered wrong on this question!`
         }
         results.append(p)
-        // longResults.append(p2)
 
     })
 
-    // includes object || object.object?? då kan vi nog ha object i arrayen för fråga 1 och 2
-    // testa lägga in object i min array sen console.logga ut just en del, alltså
-    //svarsalternativen för fråga 1.
+    let p2 = document.createElement('p')
+    p2.innerText = `You got ${totalPoints}/10 points!`
+    results.append(p2)
 
 };
 
