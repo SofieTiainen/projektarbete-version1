4c4230dc9246a0014ea66622a5bdd4f9f5aa01b3
SofieTiainen <tiainen77@hotmail.com>
2022-10-23 21:59:08 +0200

    newcommit

--- index.js
+++ index.js
@@ -1,81 +1,99 @@
-/*Switch between dark- and light mode*/
-let switchColorBtn = document.querySelector("#changeBgColor");
-switchColorBtn.addEventListener("click", () => {
-    if (switchColorBtn.innerText === "Switch to darkmode") {
-        (document.body.style.backgroundColor = "black") && (document.body.style.color = "white");
-        switchColorBtn.innerText = "Switch to lightmode";
+/*Function- Switch between dark- and light mode*/
+let switchModeBtn = document.querySelector('#swithModeBtn');
+let switchMode = () => {
+    if (switchModeBtn.innerText === 'Switch to darkmode') {
+        (document.body.style.backgroundColor = 'darkmagenta') && (document.body.style.color = 'floralwhite');
+        switchModeBtn.innerText = 'Switch to lightmode';
     } else {
-        (document.body.style.backgroundColor = "white") && (document.body.style.color = "black");
-        switchColorBtn.innerText = "Switch to darkmode";
+        (document.body.style.backgroundColor = 'floralwhite') && (document.body.style.color = 'darkmagenta');
+        switchModeBtn.innerText = 'Switch to darkmode';
     }
-});
+}
 
-// let quizAnswers = [
-//     {
-//         correctsvar1: "1. brun",
-//         correctsvar2: "1. svart"
-//     },
-//     {
-//         correctsvar3: "2. frukt",
-//         correctsvar4: "2. grönsaker"
-//     },
-//     "3. matta", "4. frukt", "5. frukt", "6. 500 gram", "7. sant", "8. sant", "9. sant", "10. sant"
-// ];
-
+/*All quizQuestions and correct answers*/
 let quizAnswers = [
     {
-        question: "Vilken färg på päls kan hundar ha?",
-        correctsvar1: "1. brun",
-        correctsvar2: "1. svart"
+        question: 'Vilken färg på päls kan hundar ha?',
+        answer1a: '1. brun',
+        answer1b: '1. svart'
     },
     {
-        question: "Vad kan man hitta i en mataffär?",
-        correctsvar3: "2. frukt",
-        correctsvar4: "2. grönsaker"
+        question: 'Vad kan man hitta i en mataffär?',
+        answer2a: '2. frukt',
+        answer2b: '2. grönsaker'
     },
     {
-        question: "Vad kan man hitta i en möbelbutik?",
-        answer: "3. matta"
+        question: 'Vad kan man hitta i en möbelbutik?',
+        answer: '3. matta'
     },
     {
-        question: "Vad är en avocado?",
-        answer: "4. frukt"
+        question: 'Vad är en avocado?',
+        answer: '4. frukt'
     },
     {
-        question: "Vad är en paprika?",
-        answer: "5. frukt"
+        question: 'Vad är en paprika?',
+        answer: '5. frukt'
     },
     {
-        question: "Hur mycket grönsaker behöver vi äta per dag?",
-        answer: "6. 500 gram"
+        question: 'Hur mycket grönsaker behöver vi äta per dag?',
+        answer: '6. 500 gram'
     },
     {
-        question: "Åsnor är dumma!",
-        answer: "7. sant"
+        question: 'Åsnor är dumma!',
+        answer: '7. sant'
     },
     {
-        question: "Hundar är snälla!",
-        answer: "8. sant"
+        question: 'Hundar är snälla!',
+        answer: '8. sant'
     },
     {
-        question: "Kött påverkar miljön negativt!",
-        answer: "9. sant"
+        question: 'Kött påverkar miljön negativt!',
+        answer: '9. sant'
     },
     {
-        question: "Väte består av 5 protoner!",
-        answer: "10. falskt"
+        question: 'Väte består av 5 protoner!',
+        answer: '10. falskt'
     }
 ];
 
 
-let showResultBtn = document.querySelector("#showResult");
-let results = document.querySelector("#results");
+let showResultBtn = document.querySelector('#showResult');
+let results = document.querySelector('#results');
+let question1Boxes = document.querySelectorAll("input[type='checkbox'][name='answer'][class='check1']");
+let question2Boxes = document.querySelectorAll("input[type='checkbox'][name='answer'][class='check2']");
 
+/*Function- choose only two checkboxes for question 1 and 2*/
+let chooseOnly2 = () => {
+    total = 0;
+    for (i = 0; i < question1Boxes.length; i++) {
+        if (question1Boxes[i].checked) {
+            total += 1;
+        }
+        if (total > 2) {
+            total = 0;
+            alert('Choose only two')
+            question1Boxes[i].checked = false;
+        }
+    }
+    total = 0;
+    for (i = 0; i < question2Boxes.length; i++) {
+        if (question2Boxes[i].checked) {
+            total += 1;
+        }
+        if (total > 2) {
+            total = 0;
+            alert('Choose only two')
+            question2Boxes[i].checked = false;
+        }
+    }
+}
 
 
 let runFiltering = () => {
     let allCheckedInputs = document.querySelectorAll("input[type='checkbox'][name='answer']:checked");
     let allCheckedRadios = document.querySelectorAll("[type='radio']:checked");
+    let totalPoints = 0;
+    let maxPoint = 10;
     let inputValues = [];
 
     allCheckedInputs.forEach((input) => {
@@ -85,27 +103,60 @@ let runFiltering = () => {
         inputValues.push(input.value);
     })
 
-    let totalPoints = 0;
+    results.innerText = '';
 
-    quizAnswers.forEach((object) => {
-        let p = document.createElement('p')
-        p.innerText = `Question: ${object.question}`
-        if (inputValues.includes(object.answer) || (inputValues.includes(object.correctsvar1)) && (inputValues.includes(object.correctsvar2)) || (inputValues.includes(object.correctsvar3)) && (inputValues.includes(object.correctsvar4))) {
-            totalPoints++
-            p.innerText += ` You answered right on this question!`
+    if (inputValues.length === 12) {
+        quizAnswers.forEach((object) => {
+            let p = document.createElement('p')
+            p.innerText = `Question: ${object.question}`
+            if (inputValues.includes(object.answer) || (inputValues.includes(object.answer1a)) && (inputValues.includes(object.answer1b)) || (inputValues.includes(object.answer2a)) && (inputValues.includes(object.answer2b))) {
+                totalPoints++
+                p.innerHTML += ` <strong>You answered right!</strong>`
+            } else {
+                p.innerHTML += ` <strong>You answered wrong!</strong>`
+            }
+            results.append(p)
+        })
+
+        let p2 = document.createElement('p')
+        p2.innerText = `You got ${totalPoints}/10 points!`
+        if (totalPoints < maxPoint * 0.5) {
+            p2.style.color = 'red';
+            p2.innerHTML += ` <strong>You failed the test</strong>`
+        } else if (totalPoints > maxPoint * 0.75) {
+            p2.style.color = 'green';
+            p2.innerHTML += ` <strong>You passed the test with special distinction</strong>`
         } else {
-            p.innerText += ` You answered wrong on this question!`
+            p2.style.color = 'orange';
+            p2.innerHTML += ` <strong>You passed the test</strong>`
         }
-        results.append(p)
+        results.append(p2)
 
-    })
+        let refreshQuizBtn = document.createElement('button')
+        refreshQuizBtn.id = 'newQuizBtn'
+        refreshQuizBtn.innerText = `Do quiz again`;
+        results.append(refreshQuizBtn)
+        refreshQuizBtn.addEventListener('click', () => {
+            location.reload();
+        })
 
-    let p2 = document.createElement('p')
-    p2.innerText = `You got ${totalPoints}/10 points!`
-    results.append(p2)
-
+    } else {
+        alert('You need to answer all questions')
+    }
 };
 
+
+switchModeBtn.addEventListener('click', () => {
+    switchMode();
+})
+
+question1Boxes.forEach((box) => {
+    box.addEventListener('change', chooseOnly2);
+})
+
+question2Boxes.forEach((box) => {
+    box.addEventListener('change', chooseOnly2);
+})
 showResultBtn.addEventListener('click', () => {
     runFiltering();
 })
