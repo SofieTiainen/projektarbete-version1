64fa0a2f0e2f2c89d3d43dd0651c63cbe2bee615
SofieTiainen <tiainen77@hotmail.com>
2022-10-25 21:29:13 +0200

    sixthcommit

--- index.js
+++ index.js
@@ -14,17 +14,11 @@ let switchMode = () => {
 let quizData = [
     {
         question: 'Vilken färg på päls kan hundar ha?',
-        answer1a: '1. brun',
-        answer1b: '1. svart',
-        answer1c: '1. rosa',
-        answer1d: '1. grön'
+        answer: ['1. brun', '1. svart', '1. rosa', '1. grön']
     },
     {
         question: 'Vad kan man hitta i en mataffär?',
-        answer2a: '2. frukt',
-        answer2b: '2. grönsaker',
-        answer2c: '2. stenar',
-        answer2d: '2. möbler'
+        answer: ['2. frukt', '2. grönsaker', '2. stenar', '2. möbler']
     },
     {
         question: 'Vad kan man hitta i en möbelbutik?',
@@ -63,6 +57,7 @@ let quizData = [
 
 let showResultBtn = document.querySelector('#showResult');
 let result = document.querySelector('#result');
+let results = document.querySelectorAll('#resultForEachQ');
 
 let getResult = () => {
     let allCheckboxesInput = document.querySelectorAll("input[type='checkbox'][name='answer']:checked");
@@ -71,6 +66,7 @@ let getResult = () => {
     let maxPoint = 10;
     let inputValues = [];
 
+
     allCheckboxesInput.forEach((input) => {
         inputValues.push(input.value);
     });
@@ -78,19 +74,29 @@ let getResult = () => {
         inputValues.push(input.value);
     })
 
+
     result.innerText = '';
+
+    /* Info för nedan i if-statement
+    (object.answer[0]) = brun och frukt (rätt)
+    (object.answer[1]) = svart och grönsaker (rätt)
+    (object.answer[1, 2]) = rosa och stenar (fel)
+    (object.answer[1, 3]) = grön och möbler (fel)*/
 
     quizData.forEach((object) => {
         let p = document.createElement('p')
         p.innerText = `Question: ${object.question}`
-        if (inputValues.includes(object.answer) || (inputValues.includes(object.answer1a)) && (inputValues.includes(object.answer1b)) && (!inputValues.includes(object.answer1c)) && (!inputValues.includes(object.answer1d)) || (inputValues.includes(object.answer2a)) && (inputValues.includes(object.answer2b)) && (!inputValues.includes(object.answer2c)) && (!inputValues.includes(object.answer2d))) {
+        if ((inputValues.includes(object.answer)) || (inputValues.includes(object.answer[0])) && (inputValues.includes(object.answer[1])) && (!inputValues.includes(object.answer[1, 2])) && (!inputValues.includes(object.answer[1, 3]))) {
             totalPoints++
-            p.innerHTML += ` <strong>You answered right!</strong>`
+            p.innerHTML += ` <strong>You answered<span style= "color: green"> right!</span</strong>`
+
         } else {
-            p.innerHTML += ` <strong>You answered wrong!</strong>`
+            p.innerHTML += ` <strong>You answered<span style= "color: red"> wrong!</span</strong>`
         }
         result.append(p)
     })
+
+
 
     let p2 = document.createElement('p')
     p2.innerText = `You got ${totalPoints}/10 points!`
@@ -99,7 +105,7 @@ let getResult = () => {
         p2.innerHTML += ` <strong>You failed the test</strong>`
     } else if (totalPoints > maxPoint * 0.75) {
         p2.style.color = 'green';
-        p2.innerHTML += ` <strong>You passed the test with special distinction</strong>`
+        p2.innerHTML += ` <strong>You passed the test with extraordinary results</strong>`
     } else {
         p2.style.color = 'orange';
         p2.innerHTML += ` <strong>You passed the test</strong>`
@@ -113,10 +119,6 @@ let getResult = () => {
     refreshQuizBtn.addEventListener('click', () => {
         location.reload();
     })
-
-
-
-
 
 }
 
